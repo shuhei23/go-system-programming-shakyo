cc6b0c64aeb8521d80c22faecac93296f82eadbc
shuhei23 <shuhei.toda@gmail.com>
2022-04-20 21:56:01 +0900

    2.4.1ファイル出力

--- 02_io_writer/main.go
+++ 02_io_writer/main.go
@@ -1,53 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"os"
 )
 
-/* interface */
-type Talker interface {
-	Talk()
-	// Talking()を入れるとGreeterで未定義なので、talker = &greeterの行でコンパイルエラーになる。
-}
-
-type Speaker interface {
-	Speak()
-}
-
-/* Greeter */
-type Greeter struct {
-	name string
-}
-
-func (g Greeter) Talk() {
-	fmt.Printf("Hello, my name is %s\n", g.name)
-}
-
-func (g Greeter) Speak() {
-	fmt.Printf("Hey Bro,  my name is %s\n", g.name)
-}
-
-/* Roarer */
-type Roarer struct {
-	name2 string
-}
-
-func (r Roarer) Talk() {
-	fmt.Printf("Hello, I'm %s\n", r.name2)
-}
-
 func main() {
-	var talker Talker
-	var speaker Speaker
-	var greeter = Greeter{"hogeo"}
-	var roarer = Roarer{"bowbow"}
-
-	speaker = &greeter
-	talker = &greeter
-	talker.Talk()
-	speaker.Speak()
-
-	//再代入すると呼び分けられる
-	talker = &roarer
-	talker.Talk()
+	file, err := os.Create("test.txt")
+	if err != nil {
+		panic(err)
+	}
+	file.Write([]byte("os.File example\n"))
+	file.Close()
 }
