4699718857f22d5157eb72186be59e136875f833
shuhei23 <shuhei.toda@gmail.com>
2022-04-20 21:23:03 +0900

    インターフェースの実験

--- 02_io_writer/main.go
+++ 02_io_writer/main.go
@@ -6,19 +6,45 @@ import (
 
 type Talker interface {
 	Talk()
+	// Talking()を入れるとGreeterで未定義なので、talker = &greeterの行でコンパイルエラーになる。
+}
+
+type Speaker interface {
+	Speak()
 }
 
 type Greeter struct {
 	name string
 }
 
+type Roarer struct {
+	name2 string
+}
+
 func (g Greeter) Talk() {
 	fmt.Printf("Hello, my name is %s\n", g.name)
 }
 
+func (g Greeter) Speak() {
+	fmt.Printf("Hey Bro,  my name is %s\n", g.name)
+}
+
+func (r Roarer) Talk() {
+	fmt.Printf("Hello, I'm %s\n", r.name2)
+}
+
 func main() {
 	var talker Talker
+	var speaker Speaker
+	var greeter = Greeter{"hogeo"}
+	var roarer = Roarer{"bowbow"}
 
-	talker = &Greeter{"hogeo"}
+	speaker = &greeter
+	talker = &greeter
+	talker.Talk()
+	speaker.Speak()
+
+	//再代入すると呼び分けられる
+	talker = &roarer
 	talker.Talk()
 }
