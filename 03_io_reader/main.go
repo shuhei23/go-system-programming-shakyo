c0b11b20a69b711e7c0839e3731e62306e3c8a6c
kazumichi oda <kazumichi_oda@outlook.jp>
2022-06-29 22:33:38 +0900

    Co-authored-by: Tomoaki777 <Tomoaki777@users.noreply.github.com> Co-authored-by: Shuhei Toda <shuhei23@users.noreply.github.com>

--- 03_io_reader/main.go
+++ 03_io_reader/main.go
@@ -2,12 +2,44 @@ package main
 
 import (
 	"io"
-	"strings"
+	"os"
 )
 
 func main() {
-	var reader io.Reader = strings.NewReader("テストデータ")
-	var readClose io.ReadCloser = io.NopCloser(reader)
+	/* 3.3.2 - 1 */
+	// var reader io.Reader = strings.NewReader("テストデータ")
+	// var readClose io.ReadCloser = io.NopCloser(reader)
 
-	print(readClose)
+	//print(readClose)
+	/* 3.3.2 - 2 */
+	// var reader = bufio.NewReader(strings.NewReader("reader"))
+	// var writer = bufio.NewWriter(os.Stdout)
+	// var readWriter io.ReadWriter = bufio.NewReadWriter(reader, writer)
+	// print(readWriter)
+
+	/* 3.4.1 */
+	// for {
+	// 	buffer := make([]byte, 5)
+	// 	size, err := os.Stdin.Read(buffer)
+	// 	if err == io.EOF {
+	// 		fmt.Println("EOF")
+	// 		break
+	// 	}
+	// 	fmt.Printf("size = %d input = '%s'\n", size, string(buffer))
+	// }
+
+	// go run main.go < main.go
+	// PowerShellでやると "<"が予約語でエラーになる
+	// コマンドプロンプトでもなぜか以下のエラーが表示されることがあった。
+	// main.go:1:1: expected 'package', found 'EOF'
+
+	/* 3.4.2 */
+	file, err := os.Open("test.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close() // defer:現在のスコープが終了したら、その後ろに書かれている行の処理を実行します
+	io.Copy(os.Stdout, file)
+
+	/* 3.4.3 */
 }
