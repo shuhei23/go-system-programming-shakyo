9fe6d68f7118ec52923750ba2d28d5512d519d8d
shuhei23 <shuhei.toda@gmail.com>
2023-01-20 21:21:11 +0900

    6.6 速度改善

--- 06.tcp/main.go
+++ 06.tcp/main.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -33,54 +34,97 @@ func HttpServer() {
 		}
 
 		go func() { // go routineによる並列処理（非同期）：クライアントごとのメッセージ受付
+			defer conn.Close()                           // クライアントと通信切断(遅延実行)
 			fmt.Printf("Accept %v\n", conn.RemoteAddr()) // クライアントのIP？
-
-			// リクエストを読み込む
-			request, err := http.ReadRequest(bufio.NewReader(conn))
-			if err != nil {
-				panic(err)
+			for {
+				//タイムアウトを設定
+				conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+				// リクエストを読み込む
+				request, err := http.ReadRequest(bufio.NewReader(conn))
+				if err != nil {
+					// タイムアウトもしくはソケットクローズ時は終了
+					// それ以外はエラーにする
+					neterr, ok := err.(net.Error) // ダウンキャスト
+					if ok && neterr.Timeout() {
+						fmt.Println("Timeout")
+						break
+					} else if err == io.EOF {
+						break
+					}
+					panic(err)
+				}
+				// リクエストを表示
+				dump, err := httputil.DumpRequest(request, true)
+				if err != nil {
+					panic(err)
+				}
+				fmt.Println(string(dump))
+				content := "Hello World\n"
+				// レスポンスを書き込む
+				response := http.Response{
+					StatusCode:    200,
+					ProtoMajor:    1,
+					ProtoMinor:    1,
+					ContentLength: int64(len(content)),
+					Body: io.NopCloser(
+						strings.NewReader(content)),
+				}
+				response.Write(conn) // ソケットに書き込み
 			}
-
-			dump, err := httputil.DumpRequest(request, true)
-			if err != nil {
-				panic(err)
-			}
-
-			fmt.Println(string(dump))
-			// レスポンスを書き込む
-			response := http.Response{
-				StatusCode: 200,
-				ProtoMajor: 1,
-				ProtoMinor: 0,
-				Body:       io.NopCloser(strings.NewReader("Hello World\n")),
-			}
-			response.Write(conn) // ソケットに書き込み
-			conn.Close()         // クライアントと通信切断
 		}()
 	}
 }
 
 func HttpClient() {
-	conn, err := net.Dial("tcp", "localhost:8888") // Listenしてるサーバーとセッション成立したら帰ってきます
-	if err != nil {
-		panic(err)
+	sendMessages := []string{
+		"ASCII",
+		"PROGRAMMING",
+		"PLUS",
 	}
+	current := 0
+	var conn net.Conn = nil
+	// リトライ用にループで全体を囲う
+	for {
+		var err error
+		// まだコネクションを張ってない / エラーでリトライ
+		if conn == nil {
+			// Dialから行ってconnを初期化
+			conn, err = net.Dial("tcp", "localhost:8888") // Listenしてるサーバーとセッション成立したら帰ってきます
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("Access: %d\n", current)
+		}
+		// POSTで文字列を送るリクエストを作成
+		request, err := http.NewRequest(
+			"POST",
+			"http://localhost:8888",
+			strings.NewReader(sendMessages[current]))
+		if err != nil {
+			panic(err)
+		}
+		err = request.Write(conn)
+		if err != nil {
+			panic(err)
+		}
+		//サーバーから読み込む。タイムアウトはここでエラーになるのでリトライ
+		response, err := http.ReadResponse(bufio.NewReader(conn), request)
+		if err != nil {
+			fmt.Println("Retry")
+			conn = nil
+			continue
+		}
+		dump, err := httputil.DumpResponse(response, true)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(dump))
+		// 全部送信完了していれば終了
+		current++
+		if current == len(sendMessages) {
+			break
+		}
 
-	request, err := http.NewRequest(
-		"Get", "http://localhost:8888", nil)
-	if err != nil {
-		panic(err)
 	}
-
-	request.Write(conn)
-	response, err := http.ReadResponse(bufio.NewReader(conn), request)
-	if err != nil {
-		panic(err)
-	}
-
-	dump, err := httputil.DumpResponse(response, true)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(dump))
+	conn.Close()
 }
